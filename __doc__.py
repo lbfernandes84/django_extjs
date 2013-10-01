69e7eba563e7d00272cef8b74ecdd1b625cfe044
Lucas Batista Fernandes <lbfernandes84@gmail.com>
2013-10-01 15:14:22 -0300

    Adicionado arquivo com introdução

---
+++ __doc__.py
@@ -0,0 +1,9 @@
+# encoding:utf-8
+    
+INTRODUCAO = """
+            Este framework foi criado com a intenção de fornecer aos desenvolvedores uma maneira de criar aplicações Django e Extjs de forma simples. É composto por 3 pacotes plugáveis, appExtjs, formExtjs e gridExtjs
+            onde são responsáveis,de forma sucinta, respectivamente por criar uma aplicação simples MVC Extjs com operações de CRUD, converter formulários django para  objetos JSON compatíveis com campos do Extjs e 
+            serializar objetos de modelos do django para serem inseridos em uma tela de listagem. Esse framework está sobre a licença Apache2, mais informações podem ser encontradas em http://httpd.apache.org/docs/2.0/license.html. 
+            Esse documento detalhará cada um dos três pacotes já citados de forma separada.
+            """
+            
